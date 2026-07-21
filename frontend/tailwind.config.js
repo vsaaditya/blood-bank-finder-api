36552f59948562884459bc21b5309bd58fee148b
Aditya raj <vsa.rajnikant@gmail.com>
2026-07-21 21:57:11 +0530

    Add Tailwind CSS configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blood: {
+          50: '#fef2f2',
+          100: '#fee2e2',
+          500: '#ef4444',
+          600: '#dc2626',
+          700: '#b91c1c',
+          900: '#7f1d1d',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
